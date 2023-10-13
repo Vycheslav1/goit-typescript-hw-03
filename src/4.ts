a7e05cd125102d1028991c668ac9d6eef0d9d11c
Vyacheslav Stetsenko <slavastetsenko@gmail.com>
2023-10-13 16:13:29 +0300

    files

--- src/4.ts
+++ src/4.ts
@@ -1,9 +1,54 @@
+class Key{
+    private signature=Math.random();
+    getSignature(){
+        return this.signature;
+    };
+}
+class Person{
+    
+    constructor(private key:number){
+       this.key=key;
+    }
+    getKey(){
+        return this.key;
+    }
+}
+
+abstract class House
+{
+public tenants:string[]=[];
+public door=true;
+private key=new Key().getSignature();
+
+constructor(key:number){
+    this.key=key;
+}
+comeIn(person:object){
+   if(this.door){
+ tenants:[...this.tenants,person];
+   } 
+}
+OpenDoor(key:number){
+     if(key===this.key){
+    console.log("door is open");
+   }
+}
+}
+class MyHouse extends House{
+   constructor(key:number){
+    super(key);
+    this.OpenDoor(key);
+    }
+  
+   
+}
+
+
 const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person(key.getSignature());
 
-house.openDoor(person.getKey());
+const house = new MyHouse(person.getKey());
 
 house.comeIn(person);
 
